62ee87fc5c369b79cce0c28f0a155fc331a3cff5
Cristian Wilgenhoff <cristian@contentment.io>
2016-02-20 16:29:18 +0000

    Added base skeleton for client app

---
+++ src/client/app/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+import UrlShortener from './components/UrlShortener';
+
+render(<UrlShortener />, document.getElementById('root'));
---
+++ src/client/app/components/UrlShortener.js
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react';
+
+class UrlShortener extends Component {
+  render(){
+    return (
+      <div className="app">
+        <h1>URL Shortener</h1>
+      </div>
+    );
+  }
+}
+
+export default UrlShortener;
