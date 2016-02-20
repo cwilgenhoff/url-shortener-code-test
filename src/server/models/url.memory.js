be9fa009a1c0d2c5d43c49d14dd58271c4c8d20a
Cristian Wilgenhoff <cristian@contentment.io>
2016-02-20 18:57:50 +0000

    Cleaned up the url model

--- src/server/models/url.memory.js
+++ src/server/models/url.memory.js
@@ -1,33 +1,41 @@
 import Promise from 'bluebird';
+import urlRegex from 'url-regex';
+import shortId from 'shortId';
 
-const url = [
+const urls = [
   {
     id: "test",
     url: "http://google.com"
   }
 ];
 
-let errorHelper = (errorMessage, reason) => {
-  return {
-    error: errorMessage,
-    reason: reason
-  };
-};
-
 export function getAll() {
-  return new Promise((resolve, reject) => {
-    resolve(url);
+  return new Promise((resolve) => {
+    resolve(urls);
   });
 }
 
 export function find(id) {
   return new Promise((resolve, reject) => {
-    reject(errorHelper(id));
+    let target = urls.find(url => url.id === id);
+
+    if (!target) {
+      reject(`Unable to find shorten URL with ID='${id}'.`);
+    }
+
+    resolve(target.url);
   });
 }
 
 export function add(url) {
-  return new Promise((resolve, reject) => {
-    reject(errorHelper(`Unable to add ${url}.`, 'Invalid URL.'));
+  return new Promise((resolve) => {
+    let urlToShorten = {
+      id: shortId.generate(),
+      url: url
+    };
+
+    urls.push(urlToShorten);
+
+    resolve(urlToShorten);
   });
 }
